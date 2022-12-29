256ed46cd66473b0c9065cd8726f78e2d3477d8e
childrentoday <baronovskidit@mail.ru>
2022-12-29 15:55:34 +0300

    Update sample1.php

--- example/sample1.php
+++ example/sample1.php
@@ -13,46 +13,24 @@ $dbname = "php_test";
 $db_table = ""; 
 
 // создать подключение к серверу
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$connect = mysqli_connect($servername, $username, $password, $dbname);
 // проверка соединения
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-
-// sql создание таблицы
-/*
-$sql = "CREATE TABLE IF NOT EXISTS MyGuests1 (
+// sql запрос на создание таблицы
+$sql = "CREATE TABLE IF NOT EXISTS Guests (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
 email VARCHAR(30),
 reg_date TIMESTAMP
 )";
-*/
 
-/*
-Если таблицы одинаковы (уже созданы), то нам нужно просто скопировать данные:
-*/
-//--1--//
-//--Копирование полностью всех данных из одной таблицы mysql в другую:--//
-/*
-$sql = "INSERT INTO `php_test`.`MyGuests1`
-SELECT *
-FROM `php_test`.`MyGuests`";
-*/
-//--2--//
-//-- Если необходимо скопировать только некоторые столбцы, то применяем следующую конструкцию: --//
-//-- Данная конструкция при изменении в таблице донора будет добавлять в таблицу реципиент новые данные каждый раз при запуске скрипта.
-//---при этом id_n будут соответствовать id исходной (донора) таблицы
-/*
-$sql = "INSERT INTO `php_test`.`MyGuests1` (`id_n`, `firstname`, `lastname`, `email`, `reg_date`)
-SELECT `id`, `firstname`, `lastname`, `email`, `reg_date`
-FROM `php_test`.`MyGuests`";
-*/
-
-if ($result = mysqli_query($conn, $sql)) {
-    echo "  Table MyGuests created successfully";
+// создать подключение к серверу
+if (mysqli_query($connect, $sql)) {
+    echo "Table Guests created successfully";
 } else {
     echo "Error creating table: " . mysqli_error($conn);
 }
